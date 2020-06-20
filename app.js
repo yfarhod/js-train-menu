7cad0c485a3c6f25cb6793314b8c81914fa6e735
yfarhod <yfarhod75@gmail.com>
2020-06-20 16:35:40 +0200

    filter buttons added in html

--- app.js
+++ app.js
@@ -76,7 +76,11 @@ const menu = [
 const sectionCenter = document.querySelector('.section-center');
 
 window.addEventListener('DOMContentLoaded', function () {
-  let displayMenu = menu.map(function (item) {
+  displayMenuItems(menu);
+});
+
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
     return `<article class="menu-item">
     <img src=${item.img} alt=${item.title} class="photo" />
     <div class="item-info">
@@ -92,4 +96,4 @@ window.addEventListener('DOMContentLoaded', function () {
   });
   displayMenu = displayMenu.join('');
   sectionCenter.innerHTML = displayMenu;
-});
+}
